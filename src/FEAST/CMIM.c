23ce5cf7a7eb4f98c09840c1ebd9a84ea1b97a65
Miron Bartosz Kursa <M.Kursa@icm.edu.pl>
2016-08-24 18:21:29 +0200

    Removal of a dead code in FEAST.

--- src/FEAST/CMIM.c
+++ src/FEAST/CMIM.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
-** CMIM.c, implements a discrete version of the 
+** CMIM.c, implements a discrete version of the
 ** Conditional Mutual Information Maximisation criterion, using the fast
 ** exact implementation from
 **
@@ -13,7 +13,7 @@
 ** Updated - 12/03/2016 - Changed initial value of maxMI to -1.0 to prevent segfaults when I(X;Y) = 0.0 for all X.
 **
 ** Author - Adam Pocock
-** 
+**
 ** Part of the FEAture Selection Toolbox (FEAST), please reference
 ** "Conditional Likelihood Maximisation: A Unifying Framework for Information
 ** Theoretic Feature Selection"
@@ -21,31 +21,31 @@
 ** Journal of Machine Learning Research (JMLR), 2012
 **
 ** Please check www.cs.manchester.ac.uk/~gbrown/fstoolbox for updates.
-** 
+**
 ** Copyright (c) 2010-2014, A. Pocock, G. Brown, The University of Manchester
 ** All rights reserved.
-** 
+**
 ** Redistribution and use in source and binary forms, with or without modification,
 ** are permitted provided that the following conditions are met:
-** 
-**   - Redistributions of source code must retain the above copyright notice, this 
+**
+**   - Redistributions of source code must retain the above copyright notice, this
 **     list of conditions and the following disclaimer.
-**   - Redistributions in binary form must reproduce the above copyright notice, 
-**     this list of conditions and the following disclaimer in the documentation 
+**   - Redistributions in binary form must reproduce the above copyright notice,
+**     this list of conditions and the following disclaimer in the documentation
 **     and/or other materials provided with the distribution.
-**   - Neither the name of The University of Manchester nor the names of its 
-**     contributors may be used to endorse or promote products derived from this 
+**   - Neither the name of The University of Manchester nor the names of its
+**     contributors may be used to endorse or promote products derived from this
 **     software without specific prior written permission.
-** 
+**
 ** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-** ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-** WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+** ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+** WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
 ** DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-** ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
-** (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
-** LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
-** ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+** ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+** (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+** LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+** ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 ** SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 **
 *******************************************************************************/
@@ -56,7 +56,7 @@
 /* MIToolbox includes */
 #include "ArrayOperations.h"
 #include "MutualInformation.h"
-  
+
 double* CMIM(int k, int noOfSamples, int noOfFeatures, double *featureMatrix, double *classColumn, double *outputFeatures)
 {
   /*holds the class MI values
@@ -65,14 +65,14 @@ double* CMIM(int k, int noOfSamples, int noOfFeatures, double *featureMatrix, do
   double *classMI = (double *)checkedCalloc(noOfFeatures,sizeof(double));
   /*in the CMIM paper, m = lastUsedFeature*/
   int *lastUsedFeature = (int *)checkedCalloc(noOfFeatures,sizeof(int));
-  
+
   double score, conditionalInfo;
-  int iMinus, currentFeature;
-  
+  int currentFeature;
+
   /*Changed to ensure it always picks a feature*/
   double maxMI = -1.0;
   int maxMICounter = -1;
-  
+
   int j,i;
 
   double **feature2D = (double**) checkedCalloc(noOfFeatures,sizeof(double*));
@@ -81,31 +81,30 @@ double* CMIM(int k, int noOfSamples, int noOfFeatures, double *featureMatrix, do
   {
     feature2D[j] = featureMatrix + (int)j*noOfSamples;
   }
-  
+
   for (i = 0; i < noOfFeatures;i++)
   {
     classMI[i] = calculateMutualInformation(feature2D[i], classColumn, noOfSamples);
-    
+
     if (classMI[i] > maxMI)
     {
       maxMI = classMI[i];
       maxMICounter = i;
     }/*if bigger than current maximum*/
   }/*for noOfFeatures - filling classMI*/
-  
+
   outputFeatures[0] = maxMICounter;
-  
+
   /*****************************************************************************
   ** We have populated the classMI array, and selected the highest
   ** MI feature as the first output feature
   ** Now we move into the CMIM algorithm
   *****************************************************************************/
-  
+
   for (i = 1; i < k; i++)
   {
     score = 0.0;
-    iMinus = i-1;
-    
+
     for (j = 0; j < noOfFeatures; j++)
     {
       while ((classMI[j] > score) && (lastUsedFeature[j] < i))
@@ -127,7 +126,7 @@ double* CMIM(int k, int noOfSamples, int noOfFeatures, double *featureMatrix, do
       }/*if partial score still greater than score*/
 	}/*for number of features*/
   }/*for the number of features to select*/
-  
+
   FREE_FUNC(classMI);
   FREE_FUNC(lastUsedFeature);
   FREE_FUNC(feature2D);
@@ -138,4 +137,3 @@ double* CMIM(int k, int noOfSamples, int noOfFeatures, double *featureMatrix, do
 
   return outputFeatures;
 }/*CMIM(int,int,int,double[][],double[],double[])*/
-
--- src/FEAST/CondMI.c
+++ src/FEAST/CondMI.c
@@ -7,7 +7,7 @@
 ** Updated - 12/03/2016 - Changed initial value of maxMI to -1.0 to prevent segfaults when I(X;Y) = 0.0 for all X.
 **
 ** Author - Adam Pocock
-** 
+**
 ** Part of the FEAture Selection Toolbox (FEAST), please reference
 ** "Conditional Likelihood Maximisation: A Unifying Framework for Information
 ** Theoretic Feature Selection"
@@ -15,31 +15,31 @@
 ** Journal of Machine Learning Research (JMLR), 2012
 **
 ** Please check www.cs.manchester.ac.uk/~gbrown/fstoolbox for updates.
-** 
+**
 ** Copyright (c) 2010-2013, A. Pocock, G. Brown, The University of Manchester
 ** All rights reserved.
-** 
+**
 ** Redistribution and use in source and binary forms, with or without modification,
 ** are permitted provided that the following conditions are met:
-** 
-**   - Redistributions of source code must retain the above copyright notice, this 
+**
+**   - Redistributions of source code must retain the above copyright notice, this
 **     list of conditions and the following disclaimer.
-**   - Redistributions in binary form must reproduce the above copyright notice, 
-**     this list of conditions and the following disclaimer in the documentation 
+**   - Redistributions in binary form must reproduce the above copyright notice,
+**     this list of conditions and the following disclaimer in the documentation
 **     and/or other materials provided with the distribution.
-**   - Neither the name of The University of Manchester nor the names of its 
-**     contributors may be used to endorse or promote products derived from this 
+**   - Neither the name of The University of Manchester nor the names of its
+**     contributors may be used to endorse or promote products derived from this
 **     software without specific prior written permission.
-** 
+**
 ** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-** ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-** WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+** ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+** WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
 ** DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-** ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
-** (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
-** LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
-** ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+** ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+** (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+** LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+** ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 ** SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 **
 *******************************************************************************/
@@ -58,82 +58,77 @@ double* CondMI(int k, int noOfSamples, int noOfFeatures, double *featureMatrix, 
 {
   /*holds the class MI values*/
   double *classMI = (double *)checkedCalloc(noOfFeatures,sizeof(double));
-  
+
   char *selectedFeatures = (char *)checkedCalloc(noOfFeatures,sizeof(char));
-  
+
   /*holds the intra feature MI values*/
   int sizeOfMatrix = k*noOfFeatures;
   double *featureMIMatrix = (double *)checkedCalloc(sizeOfMatrix,sizeof(double));
-  
+
   /*Changed to ensure it always picks a feature*/
   double maxMI = -1.0;
   int maxMICounter = -1;
-  
+
   double **feature2D = (double**) checkedCalloc(noOfFeatures,sizeof(double*));
-  
-  double score, currentScore, totalFeatureMI;
+
+  double score, currentScore;
   int currentHighestFeature;
-  
+
   double *conditionVector = (double *) checkedCalloc(noOfSamples,sizeof(double));
-  
-  int arrayPosition;
-  double mi, tripEntropy;
-  
-  int i,j,x;
-  
+
+  int i,j;
+
   for(j = 0; j < noOfFeatures; j++)
   {
     feature2D[j] = featureMatrix + (int)j*noOfSamples;
   }
-  
+
   for (i = 0; i < sizeOfMatrix;i++)
   {
     featureMIMatrix[i] = -1;
   }/*for featureMIMatrix - blank to -1*/
 
   for (i = 0; i < noOfFeatures;i++)
-  {    
+  {
     /*calculate mutual info
     **double calculateMutualInformation(double *firstVector, double *secondVector, int vectorLength);
     */
     classMI[i] = calculateMutualInformation(feature2D[i], classColumn, noOfSamples);
-    
+
     if (classMI[i] > maxMI)
     {
       maxMI = classMI[i];
       maxMICounter = i;
     }/*if bigger than current maximum*/
   }/*for noOfFeatures - filling classMI*/
-  
+
   selectedFeatures[maxMICounter] = 1;
   outputFeatures[0] = maxMICounter;
-  
+
   memcpy(conditionVector,feature2D[maxMICounter],sizeof(double)*noOfSamples);
-      
+
   /*****************************************************************************
   ** We have populated the classMI array, and selected the highest
   ** MI feature as the first output feature
   ** Now we move into the CondMI algorithm
   *****************************************************************************/
-  
+
   for (i = 1; i < k; i++)
   {
     score = 0.0;
     currentHighestFeature = -1;
     currentScore = 0.0;
-    totalFeatureMI = 0.0;
-    
+
     for (j = 0; j < noOfFeatures; j++)
     {
       /*if we haven't selected j*/
       if (selectedFeatures[j] == 0)
       {
         currentScore = 0.0;
-        totalFeatureMI = 0.0;
-        
+
         /*double calculateConditionalMutualInformation(double *firstVector, double *targetVector, double *conditionVector, int vectorLength);*/
         currentScore = calculateConditionalMutualInformation(feature2D[j],classColumn,conditionVector,noOfSamples);
-        
+
 			  if (currentScore > score)
 			  {
 				  score = currentScore;
@@ -141,23 +136,23 @@ double* CondMI(int k, int noOfSamples, int noOfFeatures, double *featureMatrix, 
 			  }
 			}/*if j is unselected*/
 	  }/*for number of features*/
-  
+
     outputFeatures[i] = currentHighestFeature;
-    
+
     if (currentHighestFeature != -1)
     {
       selectedFeatures[currentHighestFeature] = 1;
       mergeArrays(feature2D[currentHighestFeature],conditionVector,conditionVector,noOfSamples);
     }
-  
+
   }/*for the number of features to select*/
-  
+
   FREE_FUNC(classMI);
   FREE_FUNC(conditionVector);
   FREE_FUNC(feature2D);
   FREE_FUNC(featureMIMatrix);
   FREE_FUNC(selectedFeatures);
-  
+
   classMI = NULL;
   conditionVector = NULL;
   feature2D = NULL;
@@ -166,4 +161,3 @@ double* CondMI(int k, int noOfSamples, int noOfFeatures, double *featureMatrix, 
 
   return outputFeatures;
 }/*CondMI(int,int,int,double[][],double[],double[])*/
-
