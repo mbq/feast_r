cbda9aa0406dad4b4a6c01b3a2a1a2a2148d775c
Miron B. Kursa <m@mbq.me>
2017-04-22 23:28:42 +0200

    Manual upgrade

--- man-roxygen/generic.R
+++ man-roxygen/generic.R
@@ -1,4 +1,4 @@
 #' @param X Predictor table, given as a matrix or data frame with only factor or logical columns. \code{NA}s are not allowed.
 #' @param Y Decision attribute; must be either a factor or a logical vector. \code{NA}s are not allowed.
 #' @param k Number of attributes to select; first element will be used. Must not exceed \code{ncol(X)}.
-#' @return A vector of names of the selected features; note that it may be shorter than \code{k}, but should always contain at least one feature.
+#' @return A list with two elements: \code{selection}, a vector of names of the selected features (note that it may be shorter than \code{k}), and \code{scores}, a vector of feature scores.
